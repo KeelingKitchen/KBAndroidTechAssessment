2bf5d00b99fb317567f0a3b79c72af52e83e50d8
Keegan Liew <keegan.liew@datacom.co.nz>
2024-09-19 22:15:41 +1200

    feat: add transaction item text alignment

--- app/src/main/java/com/example/kbandroidtechassessment/ui/component/TransactionItem.kt
+++ app/src/main/java/com/example/kbandroidtechassessment/ui/component/TransactionItem.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.kbandroidtechassessment.data.Transaction
@@ -25,6 +26,7 @@ fun TransactionItem(transaction: Transaction) {
         Spacer(modifier = Modifier.weight(1f))
         Text(
             text = transaction.amount.toCurrencyNZDString(),
+            textAlign = TextAlign.End,
             modifier = Modifier
                 .fillMaxHeight()
                 .weight(1f)
